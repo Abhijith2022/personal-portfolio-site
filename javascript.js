3064d6e39065084c564c56207d282a3e66b7ce55
WHITEWOLF <abhijithea1@gmail.com>
2025-07-07 22:51:47 +0530

    Animation added

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
-  // document.addEventListener("DOMContentLoaded", () => {
-  //   const glow = document.querySelector(".cursor-glow");
-  //   const ring = document.querySelector(".cursor-ring");
+  document.addEventListener("DOMContentLoaded", () => {
+    const glow = document.querySelector(".cursor-glow");
+    const ring = document.querySelector(".cursor-ring");
 
-  //   window.addEventListener("mousemove", (e) => {
-  //     const x = e.clientX;
-  //     const y = e.clientY;
+    window.addEventListener("mousemove", (e) => {
+      const x = e.clientX;
+      const y = e.clientY;
 
-  //     glow.style.transform = `translate(${x - 12}px, ${y - 12}px)`;
-  //     ring.style.transform = `translate(${x - 30}px, ${y - 30}px)`;
-  //   });
-  // });
+      glow.style.transform = `translate(${x - 12}px, ${y - 12}px)`;
+      ring.style.transform = `translate(${x - 30}px, ${y - 30}px)`;
+    });
+  });

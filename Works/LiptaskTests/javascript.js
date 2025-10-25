53e32ef3c63be82a1fc94c68a1013953301db38d
Abhijith <abhijith@am.amrita.edu>
2025-10-25 23:56:54 +0530

    Liptest

---
+++ Works/LiptaskTests/javascript.js
@@ -0,0 +1,7 @@
+    const track = document.getElementById("carousel-track");
+    track.innerHTML += track.innerHTML;
+
+      function toggleMenu() {
+    document.getElementById("mobileMenuStart").classList.toggle("show");
+    document.getElementById("mobileMenuEnd").classList.toggle("show");
+  }
